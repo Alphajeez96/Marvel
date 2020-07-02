2f0739aec69bb92687f7203e97f7b2b8ceb1fa9f
Prince <chimeremezechukwudire@gmail.com>
2020-07-02 22:55:24 +0100

    navbar full_width added

--- src/App.vue
+++ src/App.vue
@@ -29,9 +29,12 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 
+}
+html, body {
+  height: 100%;
+  margin: 0;
 }
 
 
