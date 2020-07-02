20048ce52ae888408f9a49c13f6d115a6ba3aa72
Prince <chimeremezechukwudire@gmail.com>
2020-07-02 21:20:58 +0100

    navbar added

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+ <navbar> </navbar>
     </div>
     <router-view/>
+       
   </div>
 </template>
+
+<script>
+import navbar from '@/components/partials/navbar'
+
+
+export default {
+  name: 'Home',
+  components: {
+  navbar
+  }
+}
+</script>
+
 
 <style>
 #app {
@@ -14,15 +27,6 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
   color: #2c3e50;
 }
 
---
+++ src/components/partials/navbar.vue
@@ -0,0 +1,37 @@
+<template>
+   <header>
+    <nav>
+      <section class="header">
+          <div class="grid-item"><div class="grid-items">1 </div></div>
+            <div class="grid-item">1</div>
+              <div class="grid-item">1</div>
+             
+      </section>
+    </nav>
+   </header>
+</template>
+
+<style scoped>
+
+.header {
+    display: grid;
+    grid-template-columns: auto auto auto;
+    padding: 10px;
+}
+.grid-item {
+  background-color: rgba(255, 255, 255, 0.8);
+
+  padding: 20px;
+  font-size: 30px;
+  text-align: center;
+}
+.grid-items {
+  background-color: rgba(255, 255, 255, 0.8);
+  padding: 20px;
+    border: 1px solid rgba(0, 0, 0, 0.8);
+  font-size: 30px;
+  text-align: center;
+}
+
+
+</style>
